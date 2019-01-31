8a2092b87c2de60b3ecbfd9a3e00ec74b7a99c73
Scott Klassen <sklassen@localhost.localdomain>
2019-01-30 19:07:06 -0600

    remove the references to the reward

--- data_includes/rachel_data.js
+++ data_includes/rachel_data.js
@@ -2,8 +2,8 @@ var theShuffle = rshuffle2("c1-1", "c2-1", "c3-1", "c4-1")
 var theShuffle2 = rshuffle2("c1-2", "c2-2", "c3-2", "c4-2")
 
 
-var shuffleSequence1 = seq("intro", sepWith("sep", seq("pracMsg", "sepStart1", "sepStart2", "sepStart3", "sepStart4", rshuffle2("practice"), "realMsg", "sepStart1", "sepStart2", "sepStart3", "sepStart4", theShuffle, "break", "sepStart1", "sepStart2", "sepStart3", "sepStart4", theShuffle2, "sr", "lastPage")));
-var shuffleSequence2 = seq("intro", sepWith("sep", seq("pracMsg", "sepStart1", "sepStart2", "sepStart3", "sepStart4", rshuffle2("practice"), "realMsg", "sepStart1", "sepStart2", "sepStart3", "sepStart4", theShuffle2, "break", "sepStart1", "sepStart2", "sepStart3", "sepStart4", theShuffle, "sr", "lastPage")));
+var shuffleSequence1 = seq("intro", sepWith("sep", seq("pracMsg", "sepStart1", "sepStart2", "sepStart3", "sepStart4", rshuffle2("practice"), "realMsg", "sepStart1", "sepStart2", "sepStart3", "sepStart4", theShuffle, "break", "sepStart1", "sepStart2", "sepStart3", "sepStart4", theShuffle2, "sr", )));
+var shuffleSequence2 = seq("intro", sepWith("sep", seq("pracMsg", "sepStart1", "sepStart2", "sepStart3", "sepStart4", rshuffle2("practice"), "realMsg", "sepStart1", "sepStart2", "sepStart3", "sepStart4", theShuffle2, "break", "sepStart1", "sepStart2", "sepStart3", "sepStart4", theShuffle, "sr", )));
 
 function chooseShuffle(){
    i = Math.floor(Math.random() * 2);
@@ -73,10 +73,6 @@ var items = [
 
     ["sep", "Separator", { }],
     ["sep", "Separator", {normalMessage: "+"}],
-
-    ["intro", "Message", {
-      html: { include: "intro.html"}
-    }],
 
     ["intro", "Form", {
       html: { include: "ConsentFormRevised.html"}
